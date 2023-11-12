ea64c97dfe5b0c6f149e9db4a19656dd26eaf2cc
Jade Shi <ziyushi1997@gmail.com>
2023-11-12 01:40:08 -0800

    Fix padding

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <body className="app">
       <NavBar />
-      <div>
-        <div style={{padding: "1rem", paddingTop: "2rem"}}>
+      <div style={{paddingTop: "2rem", paddingBottom: "2rem"}}>
+        <div style={{padding: "1rem"}}>
           <h1 style={{ color: "#23262F", fontSize: "26px" }}>Hi Anima</h1>
           <p style={{ color: "#828282", fontSize: "16px" }}>
             See how you're shaping futures with us at NALAM!{" "}
