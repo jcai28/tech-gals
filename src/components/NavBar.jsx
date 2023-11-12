12621d3c32601e8f566262b0931f6926fbe95d1c
Jade Shi <ziyushi1997@gmail.com>
2023-11-11 20:53:22 -0800

    Add side bar content

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,61 @@
 import "./NavBar.css";
 
 export default function NavBar() {
-  return (
-    <div className='nav-bar'>
-      <h1>NavBar</h1>
-    </div>
-  )
+  const styles = {
+    logo: {
+      display: "flex",
+      gap: "1rem",
+    },
+    item: {
+      display: "flex",
+      gap: "1rem",
+      alignItems: "center",
+    },
+  };
+
+  const IconPlaceholder = () => {
+    return (
+      <div
+        style={{
+          width: "1rem",
+          height: "1rem",
+          border: "2px solid #878787",
+          borderRadius: "50%",
+        }}
+      />
+    );
+  };
+
+  const Top = () => {
+    return (
+      <div>
+        <div style={styles.logo}>
+          <img src={require("../assets/flower.png")} alt="flower" />
+          <img src={require("../assets/logo.png")} alt="nalam logo" />
+        </div>
+        <div className="items">
+          <div style={styles.item}>
+            <IconPlaceholder />
+            <p>Donor Dashboard</p>
+          </div>
+          <div style={styles.item}>
+            <IconPlaceholder />
+            <p>Donation History</p>
+          </div>
+          <div style={styles.item}>
+            <IconPlaceholder />
+            <p>Written Acknowledgements</p>
+          </div>
+          <div style={styles.item}>
+            <IconPlaceholder />
+            <p>Payment</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return <div className="nav-bar">
+    <Top/>
+  </div>;
 }
