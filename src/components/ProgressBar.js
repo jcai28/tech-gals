5e1a1cdb42c33e45dc3636c0db70baea4b761a37
Jade Shi <ziyushi1997@gmail.com>
2023-11-12 01:38:35 -0800

    Add base

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ProgressBar = ({ percentage }) => {
   const [progress, setProgress] = useState(0);
@@ -12,25 +12,21 @@ const ProgressBar = ({ percentage }) => {
   };
 
   return (
-    <div className='container'>
-        <div className="text-lg font-bold mt-2 " style={textPosition}>
+    <div className="container">
+      <div className="text-lg font-bold mt-2 " style={textPosition}>
         {`${progress}%`}
-       
-        </div>
-    <div className="m-3 text-center relative">
-        
-      <div className="h-10 border rounded-full overflow-hidden bg-gray-200 relative">
-        <div
-          className="h-full bg-green-500 transition-all duration-300 ease-in-out"
-          style={{ width: `${progress}%` }}
-        />
       </div>
-    </div>
-    <h1 className='text-right'>211,000</h1>
-  
+      <div className="m-3 text-center relative">
+        <div className="h-10 border rounded-full overflow-hidden bg-gray-200 relative" style={{height: "20px"}}>
+          <div
+            className="h-full bg-green-500 transition-all duration-300 ease-in-out"
+            style={{ width: `${progress}%` }}
+          />
+        </div>
+      </div>
+      <h1 className="text-right">211,000</h1>
     </div>
   );
 };
 
 export default ProgressBar;
-
