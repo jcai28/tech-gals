7790eeeebcfabc344f335976a411871598a09cc4
Jade Shi <ziyushi1997@gmail.com>
2023-11-11 21:50:56 -0800

    Style content

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,19 @@ export default function NavBar() {
       display: "flex",
       gap: "1rem",
     },
+    top: {
+      fontSize: 18,
+    },
     item: {
       display: "flex",
       gap: "1rem",
       alignItems: "center",
+    },
+    items: {
+      display: "flex",
+      flexDirection: "column",
+      gap: 33,
+      marginTop: 42,
     },
   };
 
@@ -26,14 +35,23 @@ export default function NavBar() {
     );
   };
 
+  const Divider = () => {
+    return (
+      <div
+        className="divider"
+        style={{ height: 1, width: "100%", backgroundColor: "#C4C4C4"}}
+      />
+    );
+  };
+
   const Top = () => {
     return (
-      <div>
+      <div style={styles.top}>
         <div style={styles.logo}>
           <img src={require("../assets/flower.png")} alt="flower" />
           <img src={require("../assets/logo.png")} alt="nalam logo" />
         </div>
-        <div className="items">
+        <div style={styles.items}>
           <div style={styles.item}>
             <IconPlaceholder />
             <p>Donor Dashboard</p>
@@ -42,20 +60,35 @@ export default function NavBar() {
             <IconPlaceholder />
             <p>Donation History</p>
           </div>
-          <div style={styles.item}>
-            <IconPlaceholder />
-            <p>Written Acknowledgements</p>
+          <div>
+            <div style={styles.item}>
+              <IconPlaceholder />
+              <p>Written</p>
+            </div>
+            <div style={styles.item}>
+              <div
+                style={{ width: "1rem", height: "1rem", visibility: "none" }}
+              />
+              <span>Acknowledgements</span>
+            </div>
           </div>
           <div style={styles.item}>
             <IconPlaceholder />
             <p>Payment</p>
+          </div>
+          <Divider />
+          <div style={styles.item}>
+            <IconPlaceholder />
+            <p>Settings</p>
           </div>
         </div>
       </div>
     );
   };
 
-  return <div className="nav-bar">
-    <Top/>
-  </div>;
+  return (
+    <div className="nav-bar">
+      <Top />
+    </div>
+  );
 }
