62a0165910ca13bd271579f2563c96175e1bfce3
jingjingcai <jcai28@usfca.edu>
2023-11-11 22:53:21 -0800

    updated girls card

--- src/components/ArrowSlider.js
+++ src/components/ArrowSlider.js
@@ -18,16 +18,17 @@ const ArrowSlider = ({ girlsData }) => {
         className="absolute top-1/2 left-2 transform -translate-y-1/2 text-2xl px-4 py-2"
         onClick={handlePrevClick}
       >
-        &lt;
+        <img src='vector.png' alt='<'></img>
       </button>
       <div className="mx-4">
         <GirlCard girl={girlsData[currentIndex]} />
+        
       </div>
       <button
         className="absolute top-1/2 right-2 transform -translate-y-1/2 text-2xl px-4 py-2"
         onClick={handleNextClick}
       >
-        &gt;
+        <img src='vector 2.png' alt='>'></img>
       </button>
     </div>
   );
--- src/components/girlCard.js
+++ src/components/girlCard.js
@@ -1,29 +1,28 @@
 import React from 'react';
-
 const GirlCard = ({ girl }) => {
-  let infoLines = ["food", "security", "education", "health"];
-  
+  let infoLines = [girl.age, girl.duration, girl.cover ]
+
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-md p-4 max-w-md mx-auto">
       <div className="flex justify-center">
-        <div className="w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 overflow-hidden rounded-full">
+        <div className="w-20 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 overflow-hidden rounded-full">
           <img className="object-cover w-full h-full" src={girl.img} alt="Profile" />
         </div>
       </div>
       <div className="text-center mt-4">
         <h1 className="text-gray-800 font-semibold text-lg md:text-xl lg:text-2xl">{girl.name}</h1>
         <hr className="my-2"></hr>
-        <p className="text-gray-600">Want to be {girl.dream}</p>
+        <p className="text-gray-600">"I want to be a {girl.dream} when I grow up”</p>
       </div>
       <div className="mt-4 text-center">
-        <h1 className="text-gray-800 font-semibold text-lg md:text-xl lg:text-2xl">1 Year</h1>
+        <h1 className="text-gray-800 font-semibold text-lg md:text-xl lg:text-2xl">${girl.price} <span className='text-sm'>USD/Month</span></h1>
         {infoLines.map((line, index) => (
           <p key={index} className="text-gray-700">{line}</p>
         ))}
       </div>
       <div className="mt-4 text-center">
-        <button className="bg-white border border-black text-black px-4 py-2 rounded-md hover:bg-gray-200 transition-all duration-300">
-          Support {girl.name}
+        <button className=" text-white px-4 py-2 rounded-md hover:bg-gray-200 transition-all duration-300" style={{backgroundColor: "#25348F"}}>
+          SUPPORT {girl.type}
         </button>
       </div>
     </div>
